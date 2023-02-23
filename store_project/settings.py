a0b77af061431370cde8b2eb9336b04a0c97a05e
Simple <Nekketsu4@mail.ru>
2023-02-23 21:35:34 +0300

    Hid the pass and set psql

--- store_project/settings.py
+++ store_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,13 +73,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'store_project.wsgi.application'
 
 
+with open('pass.txt', 'r', encoding='utf-8') as f:
+    MY_PASS = f.read().strip()
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'new_db',
+    'USER': 'postgres',
+    'PASSWORD': MY_PASS,
+    'HOST': '127.0.0.1',
+    'PORT': '5432'
     }
 }
 
