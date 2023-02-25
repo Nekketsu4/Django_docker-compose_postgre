30a68365fb78719a29eed946fbb14974fcfd28ec
Simple <Nekketsu4@mail.ru>
2023-02-25 05:06:46 +0300

    Connection between db and project

--- store_project/settings.py
+++ store_project/settings.py
@@ -79,13 +79,24 @@ with open('pass.txt', 'r', encoding='utf-8') as f:
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'new_db',
+#     'USER': 'postgres',
+#     'PASSWORD': MY_PASS,
+#     'HOST': '127.0.0.1',
+#     'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
     'ENGINE': 'django.db.backends.postgresql',
     'NAME': 'new_db',
     'USER': 'postgres',
     'PASSWORD': MY_PASS,
-    'HOST': '127.0.0.1',
+    'HOST': 'store_db',
     'PORT': '5432'
     }
 }
